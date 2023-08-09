27cd8045c67d032d71b98520d2d7904fd8cd133e
abdulrehman24 <ar5555789@gmail.com>
2023-08-09 10:45:10 +0500

    first

--- script.js
+++ script.js
@@ -22,7 +22,7 @@
             let allDetails = response.results[0].components; //passing components object to allDetails variable
             console.table(allDetails);
             let {county, postcode, country} = allDetails; //getting country, postcode, country properties value from allDetails obj
-            button.innerText = `${city} ${postcode}, ${country}`; //passing these value to the button innerText
+            button.innerText = `${county} ${postcode}, ${country}`; //passing these value to the button innerText
         }).catch(()=>{ //if error any error occured
             button.innerText = "Something went wrong";
         });
